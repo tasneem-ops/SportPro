fee57224a169b3334d0d8a458e12c5a7b60d1fd6
Tasneem Ibraheem <53406540+tasneem-ops@users.noreply.github.com>
2024-05-13 16:01:16 +0300

    League Details Design with Custom Cell

---
+++ SportPro/Modules/LeagueDetailsScreen/View/PastEventCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PastEventCollectionViewCell.swift
+//  SportPro
+//
+//  Created by JETSMobileLabMini9 on 13/05/2024.
+//
+
+import UIKit
+
+class PastEventCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ SportPro/Modules/LeagueDetailsScreen/View/UpcomingEventCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UpcomingEventCollectionViewCell.swift
+//  SportPro
+//
+//  Created by JETSMobileLabMini9 on 13/05/2024.
+//
+
+import UIKit
+
+class UpcomingEventCollectionViewCell: UICollectionViewCell {
+    
+}
